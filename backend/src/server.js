14e14b4449443190326624bb19d0abf68f36cfac
hquan187 <truonghoangquan18072005@gmail.com>
2026-05-06 07:41:05 +0700

    Update server.js
    Tăng giới hạn file ảnh upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,13 +18,14 @@ import "./models/Message.js";
 import "./models/Call.js";
 
 dotenv.config();
-  
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
-  origin: process.env.CLIENT_URL, 
-  credentials: true, 
+  origin: process.env.CLIENT_URL,
+  credentials: true,
 }));
 app.use(cookieParser());
 
